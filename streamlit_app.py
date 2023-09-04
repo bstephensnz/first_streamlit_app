11f71b11f1274ec0ef9d77740bbd8addc75aec82
Bryan Stephens <bstephens.nz@gmail.com>
2023-09-05 11:17:38 +1200

    improved display of fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -breaks the json up into key-value pairs? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - displays the normalised data
+streamlit.dataframe(fruityvice_normalized)
